b51616437b459933a87089b2994127865a955604
james <Apodisation13@gmail.com>
2021-12-17 15:05:43 +0300

    models added

--- backend/apps/cards/admin.py
+++ backend/apps/cards/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.cards.models import Faction, Color, Type, Ability, Card
+
+admin.site.register(Faction)
+admin.site.register(Color)
+admin.site.register(Type)
+admin.site.register(Ability)
+admin.site.register(Card)
--- backend/apps/cards/models.py
+++ backend/apps/cards/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Faction(models.Model):
+    name = models.CharField(max_length=32, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=32, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=32, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Ability(models.Model):
+    name = models.CharField(max_length=32, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=64, blank=False, null=False)
+    faction = models.ForeignKey(Faction, related_name='cards', on_delete=models.PROTECT)
+    color = models.ForeignKey(Color, related_name='cards', on_delete=models.PROTECT)
+    type = models.ForeignKey(Type, related_name='cards', on_delete=models.PROTECT)
+    ability = models.ForeignKey(Ability, related_name='cards', on_delete=models.PROTECT)
+    charges = models.IntegerField(default=1, blank=False, null=False)
+    hp = models.IntegerField(default=0, blank=False, null=False)
+    damage = models.IntegerField(default=0, blank=False, null=False)
+    heal = models.IntegerField(default=0, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.name}, hp {self.hp}, ability {self.ability}, damage {self.damage}, heal {self.heal} '
