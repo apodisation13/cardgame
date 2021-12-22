e5a904d71a910e716194862de0ea5610b56f3612
james <Apodisation13@gmail.com>
2021-12-22 20:03:51 +0300

    swagger try

--- backend/config/api_docs.py
+++ backend/config/api_docs.py
@@ -1,5 +1,6 @@
 from django.urls import path, re_path
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
@@ -8,18 +9,24 @@ openapi_info = openapi.Info(
         default_version='v1',
         description="Cardgame API",
     )
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
+
+
 schema_view = get_schema_view(
     openapi_info,
     public=False,
+    generator_class=BothHttpAndHttpsSchemaGenerator, # Here
     permission_classes=(permissions.AllowAny,),
 )
+
 
 urlpatterns = [
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
-
-# urlpatterns = [
-#    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-#    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-# ]
