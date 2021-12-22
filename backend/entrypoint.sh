36724c78e618f769cb9980ff31b9327138564e18
james <Apodisation13@gmail.com>
2021-12-22 16:12:02 +0300

    ff

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-bash ./wait.sh "$POSTGRES_HOST:$POSTGRES_PORT" -- python manage.py migrate --no-input
+bash wait.sh "$POSTGRES_HOST:$POSTGRES_PORT" -- python manage.py migrate --no-input
 
 bash ls -a
 
 python manage.py collectstatic
 
-exec gunicorn config.wsgi:application -b 0.0.0.0 --reload
+gunicorn config.wsgi --bind 0.0.0.0:8000
