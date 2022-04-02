114e42b0501d93b975f4834143973c7f5225aafe
james <Apodisation13@gmail.com>
2022-04-02 20:44:18 +0300

    added filters to admin

--- backend/apps/cards/admin.py
+++ backend/apps/cards/admin.py
@@ -10,7 +10,7 @@ admin.site.register(PassiveAbility)
 @admin.register(Leader)
 class LeaderAdmin(admin.ModelAdmin):
     model = Leader
-    list_filter = ("faction_id", "ability_id", "has_passive")  # по каким полям фильтр, выбор
+    list_filter = ("faction_id", "ability_id", "has_passive", "passive_ability_id")  # по каким полям фильтр, выбор
     list_display = [field.name for field in Leader._meta.fields]  # показать все поля таблицы
     list_display_links = [field.name for field in Leader._meta.fields]  # кликабельны так же все поля
 
@@ -18,7 +18,7 @@ class LeaderAdmin(admin.ModelAdmin):
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     model = Card
-    list_filter = ("faction_id", "color_id", "type_id", "ability_id", "has_passive")
+    list_filter = ("faction_id", "color_id", "type_id", "ability_id", "has_passive", "passive_ability_id")
     list_display = [field.name for field in Card._meta.fields]
     list_display_links = [field.name for field in Card._meta.fields]
 
