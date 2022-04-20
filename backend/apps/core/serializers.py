33808c6e207d418a6a911fa478d0c6d1da2545ce
james <apodisation13@gmail.com>
2022-04-20 15:13:25 +0300

    core seems to be finished

--- backend/apps/core/serializers.py
+++ backend/apps/core/serializers.py
@@ -1,9 +1,43 @@
 from rest_framework import serializers
 
-from apps.core.models import Faction
+from apps.core.models import Ability, EnemyLeaderAbility, EnemyPassiveAbility, Faction, Move, PassiveAbility
 
 
 class FactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Faction
         fields = ("name", )
+
+
+class AbilitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ability
+        fields = ("name", "description")
+
+
+class PassiveAbilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PassiveAbility
+        fields = ("name", "description")
+
+
+class MoveSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Move
+        fields = ("name", "description")
+
+
+class EnemyPassiveAbilitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = EnemyPassiveAbility
+        fields = ("name", "description")
+
+
+class EnemyLeaderAbilitySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = EnemyLeaderAbility
+        fields = ("name", "description")
