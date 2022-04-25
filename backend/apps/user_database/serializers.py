a8e61643869ed7b2f54a09c5a04351f1f42bc9b0
james <apodisation13@gmail.com>
2022-04-25 20:40:35 +0300

    another approach to user database serializer

--- backend/apps/user_database/serializers.py
+++ backend/apps/user_database/serializers.py
@@ -3,24 +3,24 @@ from rest_framework import serializers
 from apps.accounts.models import CustomUser
 from apps.cards.models import Card, Leader, UserCard, UserDeck, UserLeader
 from apps.cards.serializers import CardSerializer, DeckSerializer, LeaderSerializer
-from apps.enemies.models import Level
+from apps.enemies.models import Level, UserLevel
 from apps.enemies.serializers import LevelSerializer, UserLevelsThroughSerializer
 
 
-class UserCardsThroughSerializer(serializers.ModelSerializer):
-    card = CardSerializer(many=False)  # если это не указать,то будет просто card_id, count
-
-    class Meta:
-        model = UserCard
-        fields = ("card", "count", "id")
-
-
-class UserLeadersThroughSerializer(serializers.ModelSerializer):
-    card = LeaderSerializer(many=False, source='leader')
-
-    class Meta:
-        model = UserLeader
-        fields = ("card", "count", "id")
+# class UserCardsThroughSerializer(serializers.ModelSerializer):
+#     card = CardSerializer(many=False)  # если это не указать,то будет просто card_id, count
+#
+#     class Meta:
+#         model = UserCard
+#         fields = ("card", "count", "id")
+#
+#
+# class UserLeadersThroughSerializer(serializers.ModelSerializer):
+#     card = LeaderSerializer(many=False, source='leader')
+#
+#     class Meta:
+#         model = UserLeader
+#         fields = ("card", "count", "id")
 
 
 class UserDecksThroughSerializer(serializers.ModelSerializer):
@@ -34,17 +34,22 @@ class UserDecksThroughSerializer(serializers.ModelSerializer):
 
 class UserDatabaseSerializer(serializers.ModelSerializer):
 
-    u_c = UserCardsThroughSerializer(many=True)
-    locked_cards = serializers.SerializerMethodField()
-
-    u_l = UserLeadersThroughSerializer(many=True)
-    locked_leaders = serializers.SerializerMethodField()
+    # u_c = UserCardsThroughSerializer(many=True)
+    # locked_cards = serializers.SerializerMethodField()
+    #
+    # u_l = UserLeadersThroughSerializer(many=True)
+    # locked_leaders = serializers.SerializerMethodField()
 
     # decks = DeckSerializer(many=True)  # FIXME: а тут почему-то работает
-    u_d = UserDecksThroughSerializer(many=True)
 
-    u_level = UserLevelsThroughSerializer(many=True)
-    locked_levels = serializers.SerializerMethodField()
+    # cards = serializers.SerializerMethodField()
+    # leaders = serializers.SerializerMethodField()
+    # levels = serializers.SerializerMethodField()
+
+    u_d = UserDecksThroughSerializer(many=True)
+    #
+    # u_level = UserLevelsThroughSerializer(many=True)
+    # locked_levels = serializers.SerializerMethodField()
 
     class Meta:
         model = CustomUser
@@ -52,49 +57,73 @@ class UserDatabaseSerializer(serializers.ModelSerializer):
             "id",
             "email",
             "username",
-            "u_c",
-            "locked_cards",
-            "u_l",
-            "locked_leaders",
+            # "cards",
+            # "leaders",
+            # "levels",
+            # "u_c",
+            # "locked_cards",
+            # "u_l",
+            # "locked_leaders",
             # "decks",
-            "u_level",
-            "locked_levels",
+            # "u_level",
+            # "locked_levels",
             "u_d",
         )
 
-    def get_locked_cards(self, user):
+    def get_cards(self, user):
         user_id = user.id
-        user_locked_cards = Card.objects.\
-            select_related("faction", "color", "type", "ability", "passive_ability").\
-            exclude(u_c__user_id=user_id).\
-            all()
-        c = []
-        for u in user_locked_cards:
-            s = {'card': CardSerializer(u, context={'request': self.context.get('request')}).data, 'count': 0}
-            c.append(s)
-        return c
+        all_cards = Card.objects.select_related("faction", "color", "type", "ability", "passive_ability").all()
+        cards = []
+        for card in all_cards:
+            usercard = UserCard.objects.filter(card=card, user_id=user_id).first()
+            if usercard:
+                cards.append({
+                    "card": CardSerializer(card, context={"request": self.context["request"]} ).data,
+                    "count": usercard.count,
+                    "id": usercard.id,
+                })
+            else:
+                cards.append({
+                    "card": CardSerializer(card, context={"request": self.context["request"]}).data,
+                    "count": 0,}
+                )
+        return cards
 
-    def get_locked_leaders(self, user):
+    def get_leaders(self, user):
         user_id = user.id
-        user_locked_leaders = Leader.objects.\
-            select_related("faction", "ability", "passive_ability").\
-            exclude(u_l__user_id=user_id).\
-            all()
-        c = []
-        for u in user_locked_leaders:
-            s = {'card': LeaderSerializer(u, context={'request': self.context.get('request')}).data, 'count': 0}
-            c.append(s)
-        return c
+        all_leaders = Leader.objects.select_related("faction", "ability", "passive_ability").all()
+        leaders = []
+        for leader in all_leaders:
+            userleader = UserLeader.objects.filter(leader=leader, user_id=user_id).first()
+            if userleader:
+                leaders.append({
+                    "card": LeaderSerializer(leader, context={"request": self.context["request"]}).data,
+                    "count": userleader.count,
+                    "id": userleader.id,
+                })
+            else:
+                leaders.append({
+                    "card": LeaderSerializer(leader, context={"request": self.context["request"]}).data,
+                    "count": 0,
+                })
+        return leaders
 
-    def get_locked_levels(self, user):
+    def get_levels(self, user):
         user_id = user.id
-        user_locked_levels = Level.objects.\
+        all_levels = Level.objects.\
             select_related("enemy_leader__ability", "enemy_leader__faction").\
             prefetch_related("enemies__faction", "enemies__color", "enemies__move", "enemies__passive_ability").\
-            exclude(u_level__user_id=user_id).\
             all()
-        c = []
-        for u in user_locked_levels:
-            s = {'level': LevelSerializer(u, context={'request': self.context.get('request')}).data}
-            c.append(s)
-        return c
+        levels = []
+        for level in all_levels:
+            userlevel = UserLevel.objects.filter(level=level, user_id=user_id).first()
+            if userlevel:
+                levels.append({
+                    'level': LevelSerializer(level, context={'request': self.context.get('request')}).data,
+                    'id': userlevel.id,
+                })
+            else:
+                levels.append({
+                    'level': LevelSerializer(level, context={'request': self.context.get('request')}).data,
+                })
+        return levels
--- backend/apps/user_database/views.py
+++ backend/apps/user_database/views.py
@@ -12,21 +12,7 @@ class UserDatabaseViewSet(GenericViewSet, mixins.RetrieveModelMixin):
     authentication_classes = [TokenAuthentication]
 
     queryset = CustomUser.objects. \
-        prefetch_related("u_c__card__color",
-                         "u_c__card__type",
-                         "u_c__card__faction",
-                         "u_c__card__ability",
-                         "u_c__card__passive_ability",
-                         "u_l__leader__faction",
-                         "u_l__leader__ability",
-                         "u_l__leader__passive_ability",
-                         "u_level__level__enemies__faction",
-                         "u_level__level__enemies__color",
-                         "u_level__level__enemies__move",
-                         "u_level__level__enemies__passive_ability",
-                         "u_level__level__enemy_leader__faction",
-                         "u_level__level__enemy_leader__ability",
-                         "u_d__deck__cards__type",
+        prefetch_related("u_d__deck__cards__type",
                          "u_d__deck__cards__faction",
                          "u_d__deck__cards__ability",
                          "u_d__deck__cards__color",
@@ -34,6 +20,20 @@ class UserDatabaseViewSet(GenericViewSet, mixins.RetrieveModelMixin):
                          "u_d__deck__leader__faction",
                          "u_d__deck__leader__ability",
                          "u_d__deck__leader__passive_ability",
+                         # "u_c__card__color",
+                         # "u_c__card__type",
+                         # "u_c__card__faction",
+                         # "u_c__card__ability",
+                         # "u_c__card__passive_ability",
+                         # "u_l__leader__faction",
+                         # "u_l__leader__ability",
+                         # "u_l__leader__passive_ability",
+                         # "u_level__level__enemies__faction",
+                         # "u_level__level__enemies__color",
+                         # "u_level__level__enemies__move",
+                         # "u_level__level__enemies__passive_ability",
+                         # "u_level__level__enemy_leader__faction",
+                         # "u_level__level__enemy_leader__ability",
                          ). \
         all()
     serializer_class = UserDatabaseSerializer
