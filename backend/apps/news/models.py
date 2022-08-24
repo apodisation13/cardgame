dd4d9d77eead647b0a0cdb4b2465d745c68f5668
OksanaSha <oksanashatalova@bk.ru>
2022-08-24 16:22:37 +0300

    add field "created_at" in news/models.py

--- backend/apps/news/models.py
+++ backend/apps/news/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.id}: {self.title}'
