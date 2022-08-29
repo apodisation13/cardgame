8a4e06a8dd5b44065ce8ec77480bc65ffb7828fa
balabasta12 <balabasta12@gmail.com>
2022-08-29 19:54:37 +0300

    removed prints

--- backend/tests/tests_cards/tests_cards_models.py
+++ backend/tests/tests_cards/tests_cards_models.py
@@ -10,22 +10,18 @@ class TestCardsModels:
         # Тест метода __str__ у модели, он должен показать то, как бы она print()
         leader = Leader.objects.first()
         expected_result = 'Foltest, ability 1 - damage-one, damage 2 charges 2'
-        # print(leader)
         assert expected_result == leader.__str__()
 
     def test_cards(self):
         """Проверка на наличие карты с именем Tibor"""
         card = Card.objects.first()
         expected_result = 'Tibor'
-        # print(card)
         assert expected_result in card.__str__()
 
     def test_deck(self):
         """Проверка на наличие полей 'Bronze', 'Silver', 'Gold' и на длину строки"""
         deck = Deck.objects.first()
         expected_result = ['Bronze', 'Silver', 'Gold']
-        # print(deck)
         for i in expected_result:
             assert i in deck.__str__()
-        deck = Deck.objects.first()
         assert len(deck) == 2
