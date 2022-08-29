72ea0385b0d424876dea8d4a4aac0e806bbfff0e
balabasta12 <balabasta12@gmail.com>
2022-08-29 19:53:15 +0300

    fixed errors in length tests

--- backend/tests/tests_core/test_core_models.py
+++ backend/tests/tests_core/test_core_models.py
@@ -9,39 +9,35 @@ class TestModels:
         # Тест метода __str__ у модели, он должен показать то, как бы она print()
         faction = Faction.objects.first()
         expected_result = '1 - Neutral'
-        # print(faction)
         assert expected_result in faction.__str__()
-        assert len(faction) == 4
+        faction_all = Faction.objects.all()
+        assert len(faction_all) == 4
 
     def test_color(self):
         """Проверка цвета карт"""
         color = Color.objects.all()
         expected_result = ['Bronze', 'Silver', 'Gold']
-        # print(color)
         for i in expected_result:
             assert i in color.__str__()
 
     def test_type(self):
         """Проверка типов карт"""
-        type = Type.objects.all()
+        type_all = Type.objects.all()
         expected_result = ['Unit', 'Special']
-        # print(type)
         for i in expected_result:
-            assert i in type.__str__()
-        assert len(type) == 2
+            assert i in type_all.__str__()
+        assert len(type_all) == 2
 
     def test_ability(self):
         """Проверка атрибутов карт"""
         ability = Ability.objects.first()
         expected_result = '1 - damage-one'
-        # print(ability)
         assert expected_result == ability.__str__()
 
     def test_move(self):
         """Проверка способностей врагов ходить: down, stand, random"""
         move = Move.objects.all()
         expected_result = ['down', 'stand', 'random']
-        # print(move)
         for i in expected_result:
             assert i in move.__str__()
 
@@ -49,12 +45,10 @@ class TestModels:
         """Проверка пассивных способностей врагов"""
         enemy_passive_ability = EnemyPassiveAbility.objects.first()
         expected_result = '1 - increase-damage'
-        # print(enemy_passive_ability)
         assert expected_result == enemy_passive_ability.__str__()
 
     def test_enemy_leader_ability(self):
         """Проверка способностей лидеров врагов"""
         enemy_leader_ability = EnemyLeaderAbility.objects.first()
         expected_result = '1 - damage-once'
-        # print(enemy_leader_ability)
         assert expected_result == enemy_leader_ability.__str__()
