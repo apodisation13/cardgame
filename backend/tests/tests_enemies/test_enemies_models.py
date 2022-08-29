f6823f49674629941a6ba470b6e1a4177dc79140
balabasta12 <balabasta12@gmail.com>
2022-08-29 19:53:34 +0300

    fixed errors in length tests

--- backend/tests/tests_enemies/test_enemies_models.py
+++ backend/tests/tests_enemies/test_enemies_models.py
@@ -8,19 +8,17 @@ class TestModels:
     def test_enemy(self):
         enemy = Enemy.objects.first()
         expected_result = ['Soldiers', 'Bronze']
-        # print(enemy)
         for i in expected_result:
             assert i in enemy.__str__()
-        enemy = Enemy.objects.all()
-        assert len(enemy) == 43
+        enemy_all = Enemy.objects.all()
+        assert len(enemy_all) == 43
 
     def test_enemyleader(self):
         enemyleader = EnemyLeader.objects.all()
         expected_result = ['First Enemy Leader', 'Second Enemy Leader']
-        # print(enemyleader)
         for i in expected_result:
             assert i in enemyleader.__str__()
 
     def test_level(self):
         level = Level.objects.all()
-        print(level)
+        assert level.__str__()
