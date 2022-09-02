5524c9f46561d6eabd604fbfdc5536d99439f9c7
balabasta12 <balabasta12@gmail.com>
2022-09-02 14:58:52 +0300

    added click on title

--- backend/apps/news/admin.py
+++ backend/apps/news/admin.py
@@ -6,5 +6,6 @@ from apps.news.models import News
 @admin.register(News)
 class AdminNews(admin.ModelAdmin):
     model = News
+    list_display_links = ('title', )
     readonly_fields = ('created_at', )
     list_display = ('id', 'title', 'created_at')
