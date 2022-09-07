feeeb20a9837dce71aa2e6273336f9d130a10f52
balabasta12 <balabasta12@gmail.com>
2022-09-07 19:40:54 +0300

    fix

--- backend/tests/tests_enemies/test_enemies_models.py
+++ backend/tests/tests_enemies/test_enemies_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from apps.enemies.models import Enemy, EnemyLeader, Level
+from apps.enemies.models import Enemy, EnemyLeader
 
 
 @pytest.mark.django_db
@@ -10,15 +10,9 @@ class TestModels:
         expected_result = ['Soldiers', 'Bronze']
         for data in expected_result:
             assert data in enemy.__str__()
-        enemy_all = Enemy.objects.all()
-        assert len(enemy_all) == 43
 
     def test_enemyleader(self):
         enemyleader = EnemyLeader.objects.all()
         expected_result = ['First Enemy Leader', 'Second Enemy Leader']
         for data in expected_result:
             assert data in enemyleader.__str__()
-
-    def test_level(self):
-        level = Level.objects.all()
-        assert level.__str__()
