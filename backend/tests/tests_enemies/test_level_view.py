8c0306a6304b04c212e35bcf206cc7abfcb1b3d5
OksanaSha <oksanashatalova@bk.ru>
2022-09-11 00:33:39 +0300

    test delete_open_levels

--- backend/tests/tests_enemies/test_level_view.py
+++ backend/tests/tests_enemies/test_level_view.py
@@ -29,11 +29,36 @@ def test_unlock_levels(user_is_admin,
 @pytest.mark.django_db
 def test_unlock_levels_unauthenticated(api_client):
     body = {'related_levels': [18, 19, 20]}
-    response = api_client.post('api/v1/levels/1/unlock_levels/', data=body)
+    response = api_client.post('/api/v1/levels/1/unlock_levels/', data=body)
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_delete_open_levels_user(create_user, api_client):
+    test_user = create_user()
+    api_client.force_authenticate(test_user)
+    body = {'user': test_user.id}
+    response = api_client.delete(
+        '/api/v1/levels/delete_open_levels/',
+        data=body,
+        format='json'
+    )
     assert response.status_code == 403
 
 
-@pytest.mark.parametrize('user_is_admin', [True, False])
 @pytest.mark.django_db
-def test_delete_open_levels():
-    pass
+def test_delete_open_levels_admin(create_user, create_admin, api_client):
+    test_user = create_user()
+    user_levels = test_user.levels.count()
+    test_admin = create_admin()
+    api_client.force_authenticate(test_admin)
+    body = {'user': test_user.id}
+    response = api_client.delete(
+        '/api/v1/levels/delete_open_levels/',
+        data=body,
+        format='json'
+    )
+    user_levels_after_del = test_user.levels.count()
+    assert response.status_code == 200
+    assert user_levels_after_del == 1
+    assert response.data == f'{user_levels - 1} levels deleted'
