897938b37e94d7a56352145c12950e4bba3c9465
james <apodisation13@gmail.com>
2022-10-25 18:52:27 +0300

    test fix

--- backend/tests/tests_user_database/test_user_database.py
+++ backend/tests/tests_user_database/test_user_database.py
@@ -10,10 +10,10 @@ class TestUserDatabase:
 
     def test_aggregation_of_queries(self, create_user):
         """Проверяем эндпоинт /api/v1/user_database/1/ на объединения GET запросов"""
-        key_user_database = ["user_database", "resources", "enemies", "enemy_leaders", "game_const"]
+        key_user_database = ["user_database", "resources", "enemies", "enemy_leaders", "game_const", "seasons"]
         user = create_user()
         self.api_client.force_authenticate(user)
-        response = self.api_client.get("/api/v1/user_database/1/")
+        response = self.api_client.get(f"/api/v1/user_database/{user.id}/")
         assert response.status_code == HTTP_200_OK
         for key in key_user_database:
             assert key in response.data
