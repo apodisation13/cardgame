7d295cb704b76412ce36652eea2178ee2f7df65c
Nina Spernaskaya <nina-sp@list.ru>
2022-11-15 13:48:56 +0600

    add leaders and resources for user in admin

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -2,20 +2,30 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from apps.accounts.models import CustomUser
-from apps.cards.models import UserCard
+from apps.cards.models import UserCard, UserLeader
 from apps.enemies.models import UserLevel
 
 
 class UserCardInLine(admin.TabularInline):
     model = UserCard
+    extra = 0
 
 
 class LevelInline(admin.TabularInline):
     model = UserLevel
+    extra = 0
+
+
+class LeaderInline(admin.TabularInline):
+    model = UserLeader
+    extra = 0
 
 
 class CustomUserAdmin(UserAdmin):
-    inlines = [UserCardInLine, LevelInline, ]
+    inlines = [UserCardInLine, LeaderInline, LevelInline, ]
+    fieldsets = UserAdmin.fieldsets + (
+        ('Resources', {'fields': ('scraps', 'wood', 'kegs', 'big_kegs', 'chests', 'keys')}),
+    )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
