27c5c71f409d360bc4da9058a18bcbdf7a56ab35
Nina Spernaskaya <nina-sp@list.ru>
2022-11-18 10:45:36 +0600

    add test for craft/mill cards

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -184,6 +184,7 @@ REST_FRAMEWORK = {
     ],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 20,  # количество элементов на одной странице для пагинации
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
 USE_X_FORWARDED_HOST = True
