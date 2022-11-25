51bea654793a81874e33c06a42ef2d829b95de78
Nina Spernaskaya <nina-sp@list.ru>
2022-11-25 13:44:17 +0600

    add more tests for decks

--- backend/tests/tests_cards/test_cards_api.py
+++ backend/tests/tests_cards/test_cards_api.py
@@ -1,15 +1,14 @@
 import pytest
+import rest_framework.status as status
 from model_bakery import baker
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
 
-from apps.cards.models import Card, Leader, UserCard, UserLeader, Deck, UserDeck, CardDeck
+from apps.cards.models import Card, CardDeck, Deck, Leader, UserCard, UserDeck, UserLeader
 
 LOCKED_CARD = 1
 UNLOCKED_CARD = 2
 LOCKED_LEADER = 2
 UNLOCKED_LEADER = 1
-INITIAL_DECKS_COUNT = 1
-# INITIAL_DECK_LENGTH = 12
+BASE_DECK = 1
 
 
 @pytest.mark.django_db
@@ -27,7 +26,7 @@ class TestCardsAPI:
             u_c.save()
         response = api_client.patch(
             f"/api/v1/patchcards/mill_user_cards/{u_c.id}/")
-        assert response.status_code == HTTP_400_BAD_REQUEST, 'Нельзя уничтожить карту из стартового набора'
+        assert response.status_code == status.HTTP_400_BAD_REQUEST, 'Нельзя уничтожить карту из стартового набора'
 
     def test_mill_unlocked_leader(self, api_client, authenticated_user):
         u_l = UserLeader.objects.filter(user_id=authenticated_user.id).first()
@@ -36,13 +35,13 @@ class TestCardsAPI:
             u_l.save()
         response = api_client.patch(
             f"/api/v1/patchleaders/mill_user_leaders/{u_l.id}/")
-        assert response.status_code == HTTP_400_BAD_REQUEST, 'Нельзя уничтожить лидера из стартового набора'
+        assert response.status_code == status.HTTP_400_BAD_REQUEST, 'Нельзя уничтожить лидера из стартового набора'
 
     def test_craft_locked_card(self, api_client, authenticated_user):
         data = {'user': authenticated_user.id, 'card': LOCKED_CARD, 'count': 1}
         url = '/api/v1/patchcards/craft_user_cards/'
         response = api_client.post(url, data=data)
-        assert response.status_code == HTTP_201_CREATED, 'добавление карты'
+        assert response.status_code == status.HTTP_201_CREATED, 'добавление карты'
 
         user_cards = response.data.get('cards')
         u_c_id = None
@@ -57,7 +56,7 @@ class TestCardsAPI:
         assert record == data, 'данные записаны правильно'
 
         response = api_client.patch(f"{url}{u_c_id}/")
-        assert response.status_code == HTTP_200_OK, 'увеличение количества'
+        assert response.status_code == status.HTTP_200_OK, 'увеличение количества'
 
         user_cards = response.data.get('cards')
         count = None
@@ -72,7 +71,7 @@ class TestCardsAPI:
                 'count': 1}
         url = '/api/v1/patchleaders/craft_user_leaders/'
         response = api_client.post(url, data=data)
-        assert response.status_code == HTTP_201_CREATED, 'добавление карты'
+        assert response.status_code == status.HTTP_201_CREATED, 'добавление карты'
 
         user_leaders = response.data.get('leaders')
         u_l_id = None
@@ -87,7 +86,7 @@ class TestCardsAPI:
         assert record == data, 'данные записаны правильно'
 
         response = api_client.patch(f"{url}{u_l_id}/")
-        assert response.status_code == HTTP_200_OK, 'увеличение количества'
+        assert response.status_code == status.HTTP_200_OK, 'увеличение количества'
 
         user_leaders = response.data.get('leaders')
         count = None
@@ -104,7 +103,7 @@ class TestCardsAPI:
         url = f"/api/v1/patchcards/mill_user_cards/{u_c.id}/"
 
         response = api_client.patch(url)
-        assert response.status_code == HTTP_200_OK, 'уменьшение количества'
+        assert response.status_code == status.HTTP_200_OK, 'уменьшение количества'
 
         user_cards = response.data.get('cards')
         count = None
@@ -115,7 +114,7 @@ class TestCardsAPI:
         assert count == data['count'] - 1, 'количество уменьшилось'
 
         response = api_client.patch(url)
-        assert response.status_code == HTTP_200_OK, 'уменьшение количества до нуля'
+        assert response.status_code == status.HTTP_200_OK, 'уменьшение количества до нуля'
 
         user_cards = response.data.get('cards')
         count = None
@@ -132,7 +131,7 @@ class TestCardsAPI:
         url = f'/api/v1/patchleaders/mill_user_leaders/{u_l.id}/'
 
         response = api_client.patch(url)
-        assert response.status_code == HTTP_200_OK, 'уменьшение количества'
+        assert response.status_code == status.HTTP_200_OK, 'уменьшение количества'
 
         user_leaders = response.data.get('leaders')
         count = None
@@ -143,7 +142,7 @@ class TestCardsAPI:
         assert count == data['count'] - 1, 'количество уменьшилось'
 
         response = api_client.patch(url)
-        assert response.status_code == HTTP_200_OK, 'уменьшение количества до нуля'
+        assert response.status_code == status.HTTP_200_OK, 'уменьшение количества до нуля'
 
         user_leaders = response.data.get('leaders')
         count = None
@@ -160,24 +159,19 @@ class TestCardsAPI:
         return factory
 
     def test_create_deck(self, api_client, authenticated_user):
-        user = authenticated_user
-        decks_count = UserDeck.objects.filter(user__id=user.id).count()
-        url = f"/api/v1/decks/"
+        decks_count = UserDeck.objects.filter(user__id=authenticated_user.id).count()
+        url = "/api/v1/decks/"
         data = {'name': 'test deck', 'd': [{'card': UNLOCKED_CARD}],
                 'leader_id': UNLOCKED_LEADER}
 
         response = api_client.post(url, data)
-        print(response.data)
 
-        assert response.status_code == HTTP_201_CREATED, 'создание колоды'
-        assert len(response.data) == INITIAL_DECKS_COUNT + 1, 'количество колод юзера увеличилось'
+        assert response.status_code == status.HTTP_201_CREATED, 'создание колоды'
+        assert len(response.data) == decks_count + 1, 'количество колод юзера увеличилось'
 
     def test_update_deck(self, api_client, authenticated_user, create_user, deck_factory):
-        user = authenticated_user
-        deck = deck_factory()
-        u_d = UserDeck.objects.filter(user__id=user.id).first()
+        u_d = UserDeck.objects.create(user=authenticated_user, deck=deck_factory())
         deck_id = u_d.deck.id
-        print(f"deck_id = {deck_id}")
         deck_length = CardDeck.objects.filter(deck__id=deck_id).count()
         url = f"/api/v1/decks/{deck_id}/"
         data = {'name': 'test deck',
@@ -186,29 +180,43 @@ class TestCardsAPI:
 
         response = api_client.patch(url, data)
 
-        assert response.status_code == HTTP_200_OK, 'изменение своей колоды'
+        assert response.status_code == status.HTTP_200_OK, 'изменение своей колоды'
 
         other_user = create_user()
         api_client.force_authenticate(other_user)
-        # other_u_d = UserDeck.objects.filter(user__id=other_user.id).first()
 
         response = api_client.patch(url, data)
 
-        assert response.status_code == HTTP_403_FORBIDDEN, 'изменение чужой колоды'
+        assert response.status_code == status.HTTP_404_NOT_FOUND, 'изменение чужой колоды'
 
-    def test_delete_deck(self, api_client, authenticated_user, create_user):
-        user = authenticated_user
-        u_d = UserDeck.objects.filter(user__id=user.id).first()
+    def test_delete_deck(self, api_client, authenticated_user, create_user, deck_factory):
+        u_d = UserDeck.objects.create(user=authenticated_user, deck=deck_factory())
         deck_id = u_d.deck.id
-        url = f"/api/v1/decks/"
+        url = f"/api/v1/decks/{deck_id}/"
 
-        response = api_client.delete(f"{url}{deck_id}/")
+        response = api_client.delete(url)
 
-        assert response.status_code == HTTP_200_OK, 'удаление своей колоды'
+        assert response.status_code == status.HTTP_200_OK, 'удаление своей колоды'
 
         other_user = create_user()
-        other_u_d = UserDeck.objects.filter(user__id=other_user.id).first()
-        other_user_deck_id = other_u_d.deck.id
-        response = api_client.patch(f"{url}{other_user_deck_id}/")
+        api_client.force_authenticate(other_user)
 
-        assert response.status_code == HTTP_403_FORBIDDEN, 'удаление чужой колоды'
+        response = api_client.patch(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND, 'удаление чужой колоды'
+
+    def test_change_base_deck(self, api_client, authenticated_user):
+        url = f"/api/v1/decks/{BASE_DECK}/"
+
+        response = api_client.delete(url)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN, 'удаление базовой колоды'
+
+        deck_length = CardDeck.objects.filter(deck__id=BASE_DECK).count()
+        data = {'name': 'test deck',
+                'd': [{'card': i} for i in range(1, deck_length + 1)],
+                'leader_id': UNLOCKED_LEADER}
+
+        response = api_client.patch(url, data)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN, 'изменение базовой колоды'
