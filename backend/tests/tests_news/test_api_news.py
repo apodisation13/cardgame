69e0c3c707509fdba4a9e9c8080701843625ec8c
Nina Spernaskaya <nina-sp@list.ru>
2022-11-28 14:38:41 +0600

    add test for news other methods forbidden

--- backend/tests/tests_news/test_api_news.py
+++ backend/tests/tests_news/test_api_news.py
@@ -1,5 +1,6 @@
 import pytest
 from model_bakery import baker
+from rest_framework import status
 
 from apps.news.models import News
 
@@ -23,3 +24,22 @@ def test_get_news(user, create_user, create_news, api_client):
     response = api_client.get('/api/v1/news/')
     assert response.status_code == 200
     assert len(response.data) == len(news)
+
+
+@pytest.mark.django_db
+def test_other_news_methods(create_admin, api_client, create_news):
+    data = {'title': 'test', 'description': 'test'}
+    test_user = create_admin()
+    api_client.force_authenticate(test_user)
+
+    response = api_client.post('/api/v1/news/', data)
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED, 'создание новости'
+
+    test_news = create_news()
+    url = f"/api/v1/news/{test_news.id}/"
+
+    response = api_client.patch(url, data)
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED, 'редактирование новости'
+
+    response = api_client.delete(url)
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED, 'удаление новости'
